47a32e362429177e593ba625fcea435fee155263
TanvirAhmedOP <106543545+TanvirAhmedOP@users.noreply.github.com>
2022-10-25 19:17:15 +0600

    Add files via upload

---
+++ Lab3/PROFILE_PICTURE/Stucture_Upload.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html> <title> Upload File </title>
+<body>
+
+
+
+    <form action="upload.php" method="post" enctype="multipart/form-data"> 
+        <h2> PROFILE PICTURE </h2>
+             <img  src="64572.png" height="200px" width="200px"  title="profile pic"> </img> <br><br>
+         
+        <input type="file" name="fileToUpload" id="fileToUpload"> <br> <br>
+  
+         <hr>
+             <input type="submit" name="submit" value="Submit" >
+    </form>
+</body>
+</html>
+
